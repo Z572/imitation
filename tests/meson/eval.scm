1b2d9cff429b48dff86b330baf61b4632855c5a7
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-03-31 20:46:10 +0800

    u

--- tests/meson/eval.scm
+++ tests/meson/eval.scm
@@ -3,10 +3,9 @@
              (meson types)
              (system base compile)
              (ice-9 match))
-(define-syntax-rule (rc str)
+(define-syntax-rule (rc str ...)
   (call-with-input-string
-      (string-append str
-                     "\n")
+      (string-join (list str ...) "\n" 'suffix)
     (lambda (x)
       (call-with-prompt 'subdir_done
         (lambda ()
@@ -21,9 +20,26 @@
   (rc "project('if test', 'c')")
   (test-equal "language"
     '("c")
-    (meson-languages
-     (%meson)))
+    (meson-languages))
   (test-equal "meson-name"
     "if test"
-    (meson-name
-     (%meson))))
+    (meson-name)))
+
+(parameterize ((%meson (make <meson>)))
+  (rc "a=20"
+      "b=true"
+      "b2=false"
+      "arr=[]"
+      "dict={}")
+  (let-syntax ((check
+                (syntax-rules ()
+                  ((_ n expect varname)
+                   (test-equal n
+                     expect
+                     (variable-ref
+                      (car (assoc-ref (meson-variables)
+                                      varname))))))))
+    (check "number" 20 'a)
+    (check "bool1" #t 'b)
+    (check "bool2" #f 'b2)
+    (check "array" '() 'arr)))
